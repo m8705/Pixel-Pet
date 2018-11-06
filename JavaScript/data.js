a65ea5c27e21841bc22ef019aaeed905b5a5987d
3216005166-郭鹏燕 <1450203917@qq.com>
2018-11-06 14:12:32 +0800

    添加数值系统 1.0版本（需要再修改）

---
+++ JavaScript/data.js
@@ -0,0 +1,18 @@
+// 拓麻歌子对象属性
+var tamago = {
+    hungry : 100,
+    clean : 100,
+    happy : 100,
+    glow : 0,
+
+    isEating : 0,
+    isCleaning : 0,
+    isHappying : 0,
+}
+
+// 食物
+// var food = {
+//     apple : 15,
+//     cake : 25,
+//     rice : 35
+// }
---
+++ JavaScript/happy.js
@@ -0,0 +1,52 @@
+function happy(doing){
+    var ball = 20;
+    var TV = 25;
+    var playing = 30;
+
+    //每分钟下降心情值
+    setInterval(reduceHappy(),1000*60);
+
+    //是否玩耍
+    if(doing === ball || doing === TV || doing === playing){
+        play(object); 
+        tamago.isHappying = 0;
+    }
+}
+
+function reduceHappy(){
+    if(tamago.happy > 0){
+        tamago.happy --;
+    }else{
+        tamago.happy = 0;
+    }
+}
+
+function play(doing){
+    if(doing === ball){
+        if(tamago.happy <= 80){
+            tamago.hungry += 20;
+            tamago.isHappying = 1;
+        }else{
+            tamago.happy = 100;
+        }
+    }
+
+    if(doing === TV){
+        if(tamago.happy <= 75){
+            tamago.hungry += 25;
+            tamago.isHappying = 1;
+        }else{
+            tamago.happy = 100;
+        }
+    }
+
+    if(food === rice){
+        if(tamago.happy <=65 ){
+            tamago.happy += 35;
+            tamago.isHappying = 1;
+        }else{
+            ttamago.happy = 100;
+        }
+    }
+
+}
